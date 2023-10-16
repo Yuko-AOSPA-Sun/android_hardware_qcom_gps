CodeLinaro-mirror/femto5g_platform_hardware_qcom_gps

GPS-HAL: Add new power state for DRE engine
Add new enum field for power state which shall
indicate QDR to remain in pause state while retaining
any useful data

Change-Id: I031bbc8ecfc04ced2d68c3ea36b0b92ebfb9f053
CRs-Fixed: 3581964

gnss/GnssAdapter.cpp
@@ -30,7 +30,7 @@
 /*
 Changes from Qualcomm Innovation Center are provided under the following license:
 
-Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted (subject to the limitations in the
@@ -187,7 +187,8 @@ GnssAdapter::GnssAdapter() :
     mSendNmeaConsent(false),
     mDgnssState(0),
     mDgnssLastNmeaBootTimeMilli(0),
-    mElapsedRealTimeCal(30000000)
+    mElapsedRealTimeCal(30000000),
+    mResponseTimer(this, (LocationError)0, (uint32_t)0)
 {
     LOC_LOGD("%s]: Constructor %p", __func__, this);
     mLocPositionMode.mode = LOC_POSITION_MODE_INVALID;
@@ -2418,6 +2419,7 @@ GnssAdapter::updatePowerState(PowerStateType powerState) {
     if (POWER_STATE_UNKNOWN != powerState) {
         mPowerState = powerState;
         mLocApi->updatePowerState(mPowerState);
+        mEngHubProxy->sendPowerStateInfo(mPowerState);
     }
 }
 
@@ -6204,6 +6206,10 @@ uint32_t GnssAdapter::configDeadReckoningEngineParamsCommand(
     return sessionId;
 }
 
+void halResponseTimer:: timeOutCallback() {
+    mHal->reportResponse(mErr, mSessionID);
+}
+
 uint32_t GnssAdapter::configEngineRunStateCommand(
         PositioningEngineMask engType, LocEngineRunState engState) {
 
@@ -6236,7 +6242,15 @@ uint32_t GnssAdapter::configEngineRunStateCommand(
                     err = LOCATION_ERROR_SUCCESS;
                 }
             }
-            mAdapter.reportResponse(err, mSessionId);
+            if (LOC_ENGINE_RUN_STATE_PAUSE_RETAIN == mEngState ||
+                    LOC_ENGINE_RUN_STATE_PAUSE == mEngState) {
+               /** wait for 400msec before sending Pause/ Pause-retain acknowledgement
+                *  This allows engines to do all cleanup operations and go to pause state
+                *  On timer expiry, acknowledgement shall be sent out */
+                mAdapter.halResponseTimerStart(err, mSessionId, LOC_WAIT_TIME_MILLI_SEC);
+            } else {
+                mAdapter.reportResponse(err, mSessionId);
+            }
         }
     };
 

Yuko-AOSPA-Sun/android_hardware_qcom_gps

gnss/GnssAdapter.cpp
@@ -221,7 +221,8 @@ GnssAdapter::GnssAdapter() :
     mGnssCapabNotification{},
     mAppHash(""),
     m3GppSourceMask(QDGNSS_3GPP_SOURCE_UNKNOWN),
-    mNmeaReqEngTypeMask(LOC_REQ_ENGINE_FUSED_BIT)
+    mNmeaReqEngTypeMask(LOC_REQ_ENGINE_FUSED_BIT),
+    mResponseTimer(this, (LocationError)0, (uint32_t)0)
 {
     LOC_LOGd("Constructor %p", this);
     mLocPositionMode.mode = LOC_POSITION_MODE_INVALID;
@@ -2844,9 +2845,9 @@ GnssAdapter::updateSystemPowerState(PowerStateType systemPowerState) {
             default:
                 break;
         } // switch
-
         mLocApi->updateSystemPowerState(mSystemPowerState);
         mContext->getLBSProxyBase()->notifyPowerState(systemPowerState);
+        mEngHubProxy->sendPowerStateInfo(mSystemPowerState);
     }
 }
 
@@ -7589,6 +7590,10 @@ uint32_t GnssAdapter::configDeadReckoningEngineParamsCommand(
     return sessionId;
 }
 
+void halResponseTimer:: timeOutCallback() {
+    mHal->reportResponse(mErr, mSessionID);
+}
+
 uint32_t GnssAdapter::configEngineRunStateCommand(
     PositioningEngineMask engType, LocEngineRunState engState) {
 
@@ -7621,7 +7626,15 @@ uint32_t GnssAdapter::configEngineRunStateCommand(
                     err = LOCATION_ERROR_SUCCESS;
                 }
             }
-            mAdapter.reportResponse(err, mSessionId);
+            if (LOC_ENGINE_RUN_STATE_PAUSE_RETAIN == mEngState ||
+                    LOC_ENGINE_RUN_STATE_PAUSE == mEngState) {
+               /** wait for 400msec before sending Pause/ Pause-retain acknowledgement
+                *  This allows engines to do all cleanup operations and go to pause state
+                *  On timer expiry, acknowledgement shall be sent out */
+                mAdapter.halResponseTimerStart(err, mSessionId, LOC_WAIT_TIME_MILLI_SEC);
+            } else {
+                mAdapter.reportResponse(err, mSessionId);
+            }
         }
     };
 