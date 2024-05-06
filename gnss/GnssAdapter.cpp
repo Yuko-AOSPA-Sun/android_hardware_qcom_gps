1581dd6bb18fb2988ee85f40be0e56c3058c8a1c
Yan Huang <quic_huang@quicinc.com>
2024-05-06 11:50:01 -0700

    Add session status
    To report session status in
    LocationDataTypes Location struct
    
    Change-Id: I04baf5391b3c1c2cd20f4a7da8568e4d716a62f2
    CRs-Fixed: 3761369

--- gnss/GnssAdapter.cpp
+++ gnss/GnssAdapter.cpp
@@ -353,10 +353,15 @@ GnssAdapter::checkAndSetSPEToRunforNHz(TrackingOptions & out) {
 
 void
 GnssAdapter::convertLocation(Location& out, const UlpLocation& ulpLocation,
-                             const GpsLocationExtended& locationExtended)
+                             const GpsLocationExtended& locationExtended,
+                             loc_sess_status status)
 {
     memset(&out, 0, sizeof(Location));
     out.size = sizeof(Location);
+
+    out.flags |= LOCATION_HAS_SESSION_STATUS_BIT;
+    out.sessionStatus = status;
+
     if (LOC_GPS_LOCATION_HAS_LAT_LONG & ulpLocation.gpsLocation.flags) {
         out.flags |= LOCATION_HAS_LAT_LONG_BIT;
         out.latitude = ulpLocation.gpsLocation.latitude;
@@ -4768,7 +4773,7 @@ GnssAdapter::reportPosition(const UlpLocation& ulpLocation,
         GnssLocationInfoNotification locationInfo = {};
         list<trackingCallback> cbRunnables;
         convertLocationInfo(locationInfo, locationExtended, status);
-        convertLocation(locationInfo.location, ulpLocation, locationExtended);
+        convertLocation(locationInfo.location, ulpLocation, locationExtended, status);
         fillElapsedRealTime(locationExtended, locationInfo);
         logLatencyInfo();
 
@@ -4910,7 +4915,8 @@ GnssAdapter::reportEnginePositions(unsigned int count,
                                     engLocation->sessionStatus);
                 convertLocation(locationInfo[i].location,
                                 engLocation->location,
-                                engLocation->locationExtended);
+                                engLocation->locationExtended,
+                                engLocation->sessionStatus);
                 fillElapsedRealTime(engLocation->locationExtended,
                                     locationInfo[i]);
             }
--- location/LocationDataTypes.h
+++ location/LocationDataTypes.h
@@ -115,7 +115,7 @@ enum LocationError {
 };
 
 // Flags to indicate which values are valid in a Location
-typedef uint16_t LocationFlagsMask;
+typedef uint32_t LocationFlagsMask;
 enum LocationFlagsBits {
     LOCATION_HAS_LAT_LONG_BIT          = (1<<0), // location has valid latitude and longitude
     LOCATION_HAS_ALTITUDE_BIT          = (1<<1), // location has valid altitude
@@ -134,7 +134,8 @@ enum LocationFlagsBits {
     LOCATION_HAS_SYSTEM_TICK_BIT       = (1<<14), // location has system Tick for qtimer tick count
     LOCATION_HAS_GPTP_TIME_BIT         = (1<<15), // location has valid GPTP time
     LOCATION_HAS_GPTP_TIME_UNC_BIT     = (1<<16), // location has valid GPTP time Uncertainity
-} ;
+    LOCATION_HAS_SESSION_STATUS_BIT    = (1<<17), // location has session status
+};
 
 typedef uint16_t LocationTechnologyMask;
 // mask indicating location calculations including...
@@ -1146,10 +1147,16 @@ enum LocationQualityType {
     LOCATION_FIXED_QUALITY_TYPE = 3,
 };
 
+enum loc_sess_status {
+    LOC_SESS_SUCCESS,
+    LOC_SESS_INTERMEDIATE,
+    LOC_SESS_FAILURE
+};
 
 struct Location {
     uint32_t size;           // set to sizeof(Location)
     LocationFlagsMask flags; // bitwise OR of LocationFlagsBits to mark which params are valid
+    loc_sess_status sessionStatus; // location session status
     uint64_t timestamp;      // UTC timestamp for location fix, milliseconds since January 1, 1970
     double latitude;         // in degrees
     double longitude;        // in degrees
@@ -1174,10 +1181,8 @@ struct Location {
                                  // SPE report: confidence level is 99%
                                  // Other engine report: confidence not unspecified
     uint64_t systemTick;        // System Tick at GPS Time
-    // GPTP time field in ns
-    uint64_t elapsedgPTPTime;
-    // GPTP time Unc
-    uint64_t elapsedgPTPTimeUnc;
+    uint64_t elapsedgPTPTime;    // GPTP time field in ns
+    uint64_t elapsedgPTPTimeUnc; // GPTP time Unc
 };
 
 enum LocReqEngineTypeMask {
@@ -1542,12 +1547,6 @@ struct LLAInfo {
     double latitude;  // in degree
     double longitude; // in degree
     float altitude;  // altitude wrt to ellipsoid
-};
-
-enum loc_sess_status {
-    LOC_SESS_SUCCESS,
-    LOC_SESS_INTERMEDIATE,
-    LOC_SESS_FAILURE
 };
 
 struct GnssLocationInfoNotification {
