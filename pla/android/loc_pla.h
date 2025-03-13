741bca26a7276e6a47adba9bc7ac5d03ad897563
Saurabh Srivastava <quic_ssrivast@quicinc.com>
2025-03-14 02:29:00 +0530

    Adding API to fetch Android SDK version
    CRs-Fixed: 4081145
    Change-Id: I6cf7429fbf48e8c4653fe9c840bb5040d8550f3b

--- pla/android/loc_pla.h
+++ pla/android/loc_pla.h
@@ -27,7 +27,7 @@
  */
 /*
  * Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
- * Copyright (c) 2023-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2023-2025 Qualcomm Innovation Center, Inc. All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause-Clear
 */
 #ifndef __LOC_PLA__
@@ -66,6 +66,8 @@ extern "C" {
 #define LOC_PATH_VPE_CONF_STR      "/vendor/etc/vpeglue.conf"
 #define LOC_PATH_QPPE_CONF_STR     "/vendor/etc/qppe.conf"
 
+#define ANDROID_16_SDK_VERSION     36
+
 /*!
  * @brief Function for memory block copy
  *
@@ -91,6 +93,12 @@ static inline size_t memscpy (void *p_Dest, size_t q_DestSize, const void *p_Src
 inline int loc_boot_kpi_marker(const char * pFmt, ...)
 {
     return -1;
+}
+
+inline int loc_pla_get_android_sdk_version() {
+    char sdk_version[PROPERTY_VALUE_MAX];
+    property_get("ro.build.version.sdk", sdk_version, "0");
+    return atoi(sdk_version);
 }
 
 #ifdef __cplusplus
